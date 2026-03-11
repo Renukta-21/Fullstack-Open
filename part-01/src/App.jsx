fdaa79c0dedf7835f326bcb621730f3df7a7cb64
Edward <edu211004@gmail.com>
2026-03-10 22:16:10 -0600

    1.1 exercise

--- part-01/src/App.jsx
+++ part-01/src/App.jsx
@@ -1,13 +1,44 @@
-export default function App(){
-  const date = new Date()
-  return(
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
     <div>
-        <h3><Greeting name={'Daniel'}/></h3>
-      {date.toString()}
-  </div>
+      <Header course={course} />
+
+      <Content part={part1} content={exercises1}/>
+      <Content part={part2} content={exercises2}/>
+      <Content part={part3} content={exercises3}/>
+
+      <Total 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
   )
 }
 
-const Greeting = ({name})=>{
-  return <div>Hello {name}</div>
+const Header = ({course}) => {
+  return <h1>{course}</h1>
 }
+
+const Content = ({part, content}) => {
+  return(
+    <div>
+      <h2>{part}</h2>
+      <p>{content}</p>
+    </div>
+  )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return <p>Total of exercises {exercises1 + exercises2 + exercises3}</p>
+}
+
+export default App
