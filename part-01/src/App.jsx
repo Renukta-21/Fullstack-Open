1caf3e67fde2932460944cd7ff4f35173eff84a2
Edward <edu211004@gmail.com>
2026-03-13 10:14:34 -0600

    1.4 exercise

--- part-01/src/App.jsx
+++ part-01/src/App.jsx
@@ -1,24 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course}/>
-      <Part part={part1}/>
-      <Part part={part2}/>
-      <Part part={part3}/>
+      {parts.map(p=> <p>{p.name}-{p.exercises}</p> )}
     </div>
   )
 }
