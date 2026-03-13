84a646a8daddd6ca2d9a2af2dcc24e3dd058d062
Edward <edu211004@gmail.com>
2026-03-13 10:28:58 -0600

    managing counters

--- part-01/src/App.jsx
+++ part-01/src/App.jsx
@@ -1,35 +1,14 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+import { useState } from "react"
 
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  setTimeout(()=>{
+    setCounter(counter+1)
+  }, 1000)
   return (
-    <div>
-      <Header name={course.name}/>
-      {course.parts.map(p=> <Part part={p}/>)}
-    </div>
+    <div>{counter}</div>
   )
-}
-
-const Header = ({name})=>{
-  return <h1>{name}</h1>
-}
-const Part = ({part})=>{
-  return <p>{part.name} {part.exercises}</p>
 }
 
 export default App
