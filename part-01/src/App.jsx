2100072a49a4519b64b99374fd82e859a25c7703
Edward <edu211004@gmail.com>
2026-03-13 10:11:33 -0600

    1.3 exercise

--- part-01/src/App.jsx
+++ part-01/src/App.jsx
@@ -1,46 +1,33 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course}/>
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Part part={part1}/>
+      <Part part={part2}/>
+      <Part part={part3}/>
     </div>
   )
 }
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
-  return (
-    <div>
-      <Part name={part1} number={exercises1} />
-      <Part name={part2} number={exercises2} />
-      <Part name={part3} number={exercises3} />
-    </div>
-  )
-}
 const Header = ({name})=>{
-  return <h3>{name}</h3>
+  return <h1>{name}</h1>
 }
-
-const Part = ({ name, number }) => {
-  return <p>{number} {name}</p>
-}
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Total of exercises {exercises1 + exercises2 + exercises3}</p>
+const Part = ({part})=>{
+  return <p>{part.name} {part.exercises}</p>
 }
 
 export default App
