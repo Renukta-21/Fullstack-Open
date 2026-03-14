25498b59307b508bd1b4368034762f5dba773e12
Edward <edu211004@gmail.com>
2026-03-14 14:37:24 -0600

    2.8

--- part-02/src/components/Phonebook.jsx
+++ part-02/src/components/Phonebook.jsx
@@ -3,38 +3,51 @@ import './phonebook.css'
 
 function Phonebook() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '5640548811' }
   ])
   const [userInput, setUserInput] = useState('')
+  const [phone, setPhone] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(persons.some(p=> p.name===userInput)){
+    if (persons.some(p => p.name === userInput)) {
       alert(`${userInput} already exists`)
       return
     }
     const newPerson = {
-      name: userInput
+      name: userInput,
+      phone
     }
     setPersons(persons.concat(newPerson))
     setUserInput('')
+    setPhone('')
   }
   return (
     <div>
       <h3>Phonebook</h3>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="inputNewUser">name</label>
-        <input type="text" id="inputNewUser" value={userInput} onChange={(e)=> setUserInput(e.target.value)}/>
+        <div>
+          <label htmlFor="inputNewUser">name</label>
+          <input type="text" id="inputNewUser" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="inputPhone">Phone</label>
+          <input type="text" id="inputPhone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        </div>
         <button>Add</button>
       </form>
       <table>
         <thead>
           <tr>
             <th>Name</th>
+            <th>Phone</th>
           </tr>
         </thead>
         <tbody>
-          {persons.map(p => <tr key={p.name}><td>{p.name}</td></tr>)}
+          {persons.map(p => <tr key={p.name}>
+            <td>{p.name}</td>
+            <td>{p.phone}</td>
+          </tr>)}
         </tbody>
       </table>
     </div>
