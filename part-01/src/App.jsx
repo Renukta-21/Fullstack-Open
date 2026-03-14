9deb99d73f6a285177e3e102a717aa5c8b762bb2
Edward <edu211004@gmail.com>
2026-03-13 21:39:23 -0600

    1.7

--- part-01/src/App.jsx
+++ part-01/src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   const [bad, setBad] = useState(0)
   const [neutral, setNeutral] = useState(0)
 
-  const handleGoodClick = ()=> setGood(good+1)
-  const handleNeutralClick = ()=> setNeutral(neutral+1)
-  const handlebadClick = ()=> setBad(bad+1)
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handlebadClick = () => setBad(bad + 1)
 
+  const total = good + bad + neutral
   return (
     <div>
       <h1>give feedback</h1>
@@ -21,12 +22,18 @@ function App() {
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
       </div>
-      <Average good={good} neutral={neutral} bad={bad}/>
+      <Average good={good} neutral={neutral} bad={bad} total={total}/>
+      <Positive good={good} total={total}/>
     </div>
   )
 }
-const Average = ({good, neutral, bad})=>{
-  return <p>{(good * 1+neutral*0+bad*-1)/3}</p>
+const Average = ({ good, neutral, bad, total }) => {
+  return <p>{(good * 1 + neutral * 0 + bad * -1) / total}</p>
+}
+const Positive = ({ good, total }) => {
+  if(total==0) return <p>No posite reviews yet</p>
+
+  return <p>Positive {(good/total) * 100}</p>
 }
 
 export default App
