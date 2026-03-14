179eb20db304126a0a8d2c3d068db497c2fd61c6
Edward <edu211004@gmail.com>
2026-03-14 17:29:11 -0600

    fetching

--- part-02/src/components/Notes.jsx
+++ part-02/src/components/Notes.jsx
@@ -1,16 +1,24 @@
-function Notes({notes}) {
-  if(!notes) return <p>No notes yet!</p>
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+function Notes() {
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3001/notes")
+            .then(response => setNotes(response.data))
+    })
+    if (notes.length===0) return <p>No notes yet!</p>
     return (
-    <div>
-        <p>
-            {notes.map(n=> <div>
-                <small>{n.id}</small>
-                <p>{n.content}</p>
-                <p>{n.important.toString()}</p>
-            </div> )}
-        </p>
-    </div>
-  )
+        <div>
+            <p>
+                {notes.map(n => <div>
+                    <small>{n.id}</small>
+                    <p>{n.content}</p>
+                    <p>{n.important.toString()}</p>
+                </div>)}
+            </p>
+        </div>
+    )
 }
 
 export default Notes
--- part-02/src/main.jsx
+++ part-02/src/main.jsx
@@ -6,14 +6,11 @@ import Phonebook from './components/Phonebook.jsx'
 import Notes from './components/Notes.jsx'
 import axios from "axios"
 
-axios.get("http://localhost:3001/notes").then(response=>{
-  const notes = response.data
   createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Notes notes={notes}/>
+    <Notes/>
     {/* <Phonebook/> */}
   </StrictMode>,
 )
-})
 
