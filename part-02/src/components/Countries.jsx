a160275de58dcf4f2f5cc10106e807f475758d17
Edward <edu211004@gmail.com>
2026-03-16 22:22:52 -0600

    exercise pending

---
+++ part-02/src/components/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Countries() {
+  return (
+    <div>
+        <p>No content at this moment because API failed</p>
+    </div>
+  )
+}
+
+export default Countries
--- part-02/src/main.jsx
+++ part-02/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import Phonebook from './components/Phonebook.jsx'
 import Notes from './components/Notes.jsx'
+import Countries from './components/Countries.jsx'
+
 
   createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <Notes/> */}
-    <Phonebook/>
+    {/* <Phonebook/> */}
+    <Countries/>
   </StrictMode>,
 )
 
