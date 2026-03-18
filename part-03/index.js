0d84113101a261e23dff84e780283b674a8ee12a
Edward <edu211004@gmail.com>
2026-03-17 22:22:03 -0600

    handling delete

--- part-03/index.js
+++ part-03/index.js
@@ -3,38 +3,51 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 const persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Main API</h1>`)
+  res.send(`<h1>Main API</h1>`)
 })
-app.get('/info', (req,res)=>{
+app.get('/info', (req, res) => {
   res.send(`Phonebook has info for ${persons.length} people\n ${new Date()}`)
 })
-app.get('/api/persons', (req,res)=>{
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  const person = persons.find(p => p.id === id)
+  if (!person) return res.status(404).send({ error: 'Person was not find' })
+  res.send(person)
+})
+app.delete('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  const personIndex = persons.findIndex(p => p.id === id)
+  if (personIndex == -1) return res.status(404).json({ message: 'User does not exist' })
+  persons.splice(personIndex, 1)
   res.json(persons)
 })
 
 app.listen(PORT, () => {
-    console.log(`App running on ${PORT}`)
+  console.log(`App running on ${PORT}`)
 })
