e8911eb3ed4b8d4eb3f545652a4ff47244833063
Edward <edu211004@gmail.com>
2026-03-20 22:59:08 -0600

    managing fetch

--- part-03/services/mongo.js
+++ part-03/services/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const password = process.argv[2]
 
-const MONGO_URI = `mongodb+srv://fullstack:${password}@cluster0.nd6u1.mongodb.net/?appName=Cluster0`
+const MONGO_URI = `mongodb+srv://fullstack:${password}@cluster0.nd6u1.mongodb.net/settingMongoDB?appName=Cluster0`
 mongoose.connect(MONGO_URI)
     .then(response => console.log('Connected succesfully to mongoDB'))
     .catch(err => console.error(err))
@@ -14,10 +14,13 @@ const userSchema = mongoose.Schema({
 
 const User = new mongoose.model('User', userSchema)
 const newUser = new User({
-    name: 'Eduardo Martinez',
-    age: 21
+    name: 'Daniel Urbina',
+    age: 23
 })
 
 newUser.save()
     .then(response => console.log(response))
     .catch(err => console.log(error))
+User.find({})
+.then(result=> console.log(result))
+.catch(err=> console.log(err))
